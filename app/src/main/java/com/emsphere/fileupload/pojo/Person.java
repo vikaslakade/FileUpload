1e8576c77dbbd18ddba172d7e8982b43bcd0dc68
SEEDINFOTECH\commando4 <vikaslakade01@gmail.com>
2018-12-20 09:57:13 +0530

    Person Pojo Added

---
+++ app/src/main/java/com/emsphere/fileupload/pojo/Person.java
@@ -0,0 +1,36 @@
+package com.emsphere.fileupload.pojo;
+
+/**
+ * Created by commando4 on 12/20/2018.
+ */
+
+public class Person {
+
+    String id;
+    String Email;
+    String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
